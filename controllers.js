3130547240aa9f540fc26eaea30bf9b03b6f588b
Mark Simoncic <63131950+masimoncic@users.noreply.github.com>
2021-04-02 15:10:06 -0400

    generateTruthTable/ submit route

--- controllers.js
+++ controllers.js
@@ -9,25 +9,39 @@ module.exports.projects = async (req, res) => {
 }
 
 module.exports.generateTruthTable = async (req, res) => {
-  res.render('generateTruthTable');
+  try {
+    res.render('generateTruthTable', { table });
+  } catch {
+    let table = null;
+    res.render('generateTruthTable', { table });
+  }
 }
 
+
+
 module.exports.submit = async (req, res) => {
-  function createTable(array) {
-    var table = document.createElement('table');
-    for (var i = 0; i < array.length; i++) {
-        var row = document.createElement('tr');
-        for (var j = 0; j < array[i].length; j++) {
-            var cell = document.createElement('td');
-            cell.textContent = array[i][j];
-            row.appendChild(cell);
-        }
-        table.appendChild(row);
+  if (req.body.propositions) {
+    let propositions = req.body.propositions;
+    const filteredProps = [];
+    for (i = 0; i < propositions.length; i++) {
+      if (propositions[i] !== '') {
+        console.log(propositions[i])
+        filteredProps.push(propositions[i]);
+      }
     }
-    return table;
+    let table = generateTruthTable(filteredProps);
+    res.render('generateTruthTable', { table })
+  } else {
+    let table = null;
+    res.render('generateTruthTable', { table })
   }
 
-  console.log(req.body);
-  res.redirect('/generateTruthTable');
-
+/*  try {
+    const { propositions } = req.body;
+    let table = generateTruthTable(propositions)
+    res.render('generateTruthTable', { table });
+  } catch {
+    table = null;
+    res.render('generateTruthTable', { table });
+  }*/
 }
--- public/javascripts/createInputField.js
+++ public/javascripts/createInputField.js
@@ -1,21 +1,16 @@
 let propCount = 1;
 function createInputField() {
-  let div = document.createElement('div');
-  div.id = `input${propCount}`;
+
   let input = document.createElement('input');
   input.type = 'text';
   input.id = `prop${propCount}`;
   input.name = `propositions[${propCount}]`;
-  let label = document.createElement('label');
-  label.for = input.id;
-  label.innerHTML = 'Enter a proposition';
-  div.appendChild(label);
-  div.appendChild(input);
+  input.setAttribute("class", "form-control mb-3");
+
   propInputs = document.getElementById('propInputs');
-  propInputs.appendChild(div);
+  propInputs.appendChild(input);
   propCount ++;
 }
 
 let addProp = document.getElementById('addProp');
 addProp.addEventListener('click', createInputField);
-
