871f8c32246e04b6eb247c045a69b8db8a016157
Mark Simoncic <63131950+masimoncic@users.noreply.github.com>
2021-04-03 16:12:07 -0400

    table style and basic error handling

--- controllers.js
+++ controllers.js
@@ -10,30 +10,38 @@ module.exports.projects = async (req, res) => {
 
 module.exports.generateTruthTable = async (req, res) => {
   try {
-    res.render('generateTruthTable', { table });
+    res.render('generateTruthTable', { table, filteredProps });
   } catch {
     let table = null;
-    res.render('generateTruthTable', { table });
+    let filteredProps = null;
+    let valid = true;
+    res.render('generateTruthTable', { table, filteredProps, valid });
   }
 }
 
 
 
 module.exports.submit = async (req, res) => {
+  let table = null;
+  let filteredProps = null;
+  let valid = true;
   if (req.body.propositions) {
     let propositions = req.body.propositions;
-    const filteredProps = [];
+    let filteredProps = [];
     for (i = 0; i < propositions.length; i++) {
       if (propositions[i] !== '') {
-        console.log(propositions[i])
         filteredProps.push(propositions[i]);
       }
     }
-    let table = generateTruthTable(filteredProps);
-    res.render('generateTruthTable', { table })
+    try { 
+      table = generateTruthTable(filteredProps);
+      res.render('generateTruthTable', { table, filteredProps, valid })
+    } catch {
+      valid = false;
+      res.render('generateTruthTable', { table, filteredProps, valid })
+    }
   } else {
-    let table = null;
-    res.render('generateTruthTable', { table })
+    res.render('generateTruthTable', { table, filteredProps, valid })
   }
 
 /*  try {
